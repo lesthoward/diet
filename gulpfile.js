b1f4b9f2eb6f5cb7b478c65f7315c5fef3b3f0bf
lesthoward <lesthoward@gmail.com>
2021-08-17 11:23:43 -0600

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const browserSync = require('browser-sync').create()
+
+// Luego de crear los estilos css, llama la función de sincronizar navegador
+function crearEstilos () {
+    return gulp
+        .src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./styles'))
+        .pipe(browserSync.stream())
+}
+
+function sincronizarServidor () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        browser: 'firefox'
+    })
+}
+
+function observarCambios () {
+    sincronizarServidor ()
+    gulp.watch('./scss/*.scss', crearEstilos)
+    gulp.watch('./**/*.html').on('change', browserSync.reload)
+    gulp.watch('./**/*.css').on('change', browserSync.reload)
+    gulp.watch('./**/*.js').on('change', browserSync.reload)
+}
+
+gulp.task('sync', gulp.parallel(observarCambios))
---
+++ scripts/crearDieta.js
@@ -0,0 +1,56 @@
+import {Formulario, FormularioDocumento} from '../scripts/clases.js' 
+
+const formulariosContenedor = document.querySelector('.formulario__envoltura');
+const formulario = new Formulario();
+const formularioDocumento = new FormularioDocumento();
+// Cargar formulario por defecto
+formularioDocumento.trasladarFormulario(-1)
+
+
+// Efecto para trasladar el formulario
+const eventosFormulario = (e) => {
+    e.preventDefault();
+    const objetivo = e.target 
+    // Si el boton siguiente es presionado llama a mi clase de formulario con eventos y la formulario para pintar en el documento
+    if(objetivo.classList.contains('formulario__siguiente')) {
+        const pasos = formulario.formularioSiguiente()
+        formularioDocumento.trasladarFormulario(pasos)
+    }
+
+    if(objetivo.classList.contains('formulario__atras')) {
+        const pasos = formulario.formularioAnterior()
+        formularioDocumento.trasladarFormulario(pasos)
+    }
+}
+
+formulariosContenedor.addEventListener('click', eventosFormulario)
+
+
+
+// personaObj = {
+//     id: Date.now(),
+//     nombre: 'Wilmer',
+//     apellido: 'Howard',
+//     pass: '123456',
+//     correo: 'wilmer@hispanoamericana.com',
+//     edad: 22,
+//     medidasCuerpo: {
+//         pecho: '',
+//         hombros: '',
+//         biceps: '',
+//         cintura: '',
+//         gluteo: '',
+//         piernas: ''
+//     },
+//     objetivo: 'ganar peso',
+//     tipoCuerpo: 'ectomorfo',
+//     tipoTrabajo: 'oficina',
+//     tipoActividades: 'correr',
+//     tipoActividadesFrecuencia: '3 veces a la semana',
+//     adiccion: 'fumar',
+//     adiccionFrecuencia: 'diario',
+//     limitacionFisica: 'Asma',
+//     consumoDiarioAgua: '1L',
+//     comentariosAdicionales: '',
+//     recomendaciones: 'Tienes que tomar más carbohidratos'
+// }
